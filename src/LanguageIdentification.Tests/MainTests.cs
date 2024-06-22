a943edbd944f98865f225da08f6ce6237a4faded
Alexander Gluschenko <qugluschenko@gmail.com>
2024-06-22 14:01:35 +0400

    Create LanguageIdentification.Tests project + test env

---
+++ src/LanguageIdentification.Tests/MainTests.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageIdentification.Tests;
+
+public class MainTests
+{
+    [Fact]
+    public void CheckRunningUnix()
+    {
+        Assert.Equal(PlatformID.Unix, Environment.OSVersion.Platform);
+        Assert.Equal("true", Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER"));
+    }
+}
---
+++ src/test.Dockerfile
@@ -0,0 +1,5 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:8.0-bookworm-slim
+
+RUN wget https://aka.ms/getvsdbgsh && \
+    sh getvsdbgsh -v latest  -l /vsdbg
+
