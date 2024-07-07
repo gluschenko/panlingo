03211d5b56578ae890f6bda82a0a6ef7cee5fc11
Alexander Gluschenko <qugluschenko@gmail.com>
2024-07-07 15:18:36 +0400

    CLD3: remove semaphore in prediction methods

--- src/LanguageIdentification.CLD3/CLD3Detector.cs
+++ src/LanguageIdentification.CLD3/CLD3Detector.cs
@@ -18,7 +18,6 @@ public class CLD3Detector : IDisposable
         try
         {
             _semaphore.Wait();
-
             GC.SuppressFinalize(this);
             CLD3DetectorWrapper.FreeIdentifier(_identifier);
         }
@@ -30,16 +29,7 @@ public class CLD3Detector : IDisposable
 
     public CLD3PredictionResult FindLanguage(string text)
     {
-        try
-        {
-            _semaphore.Wait();
-
-            return CLD3DetectorWrapper.FindLanguage(_identifier, text);
-        }
-        finally
-        {
-            _semaphore.Release();
-        }
+        return CLD3DetectorWrapper.FindLanguage(_identifier, text);
     }
 
     public CLD3PredictionResult[] FindLanguageNMostFreqLangs(
@@ -47,37 +37,28 @@ public class CLD3Detector : IDisposable
         int numLangs
     )
     {
+        var resultPtr = CLD3DetectorWrapper.FindTopNMostFreqLangs(
+            identifier: _identifier,
+            text: text,
+            numLangs: numLangs,
+            resultCount: out var resultCount
+        );
+
         try
         {
-            _semaphore.Wait();
+            var result = new CLD3PredictionResult[resultCount];
+            var structSize = Marshal.SizeOf(typeof(CLD3PredictionResult));
 
-            var resultPtr = CLD3DetectorWrapper.FindTopNMostFreqLangs(
-                identifier: _identifier,
-                text: text,
-                numLangs: numLangs,
-                resultCount: out var resultCount
-            );
-
-            try
+            for (var i = 0; i < resultCount; i++)
             {
-                var result = new CLD3PredictionResult[resultCount];
-                var structSize = Marshal.SizeOf(typeof(CLD3PredictionResult));
-
-                for (var i = 0; i < resultCount; i++)
-                {
-                    result[i] = Marshal.PtrToStructure<CLD3PredictionResult>(resultPtr + i * structSize);
-                }
-
-                return result;
+                result[i] = Marshal.PtrToStructure<CLD3PredictionResult>(resultPtr + i * structSize);
             }
-            finally
-            {
-                CLD3DetectorWrapper.FreeResults(resultPtr, resultCount);
-            }
+
+            return result;
         }
         finally
         {
-            _semaphore.Release();
+            CLD3DetectorWrapper.FreeResults(resultPtr, resultCount);
         }
     }
 }
