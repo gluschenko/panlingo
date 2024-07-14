f3b533666299aa3ef9772332d3e4f06aadf9cb2b
Alexander Gluschenko <qugluschenko@gmail.com>
2024-07-14 19:06:41 +0400

    Fixes

--- src/LanguageIdentification.FastText.Native/FastTextNativeLibrary.cs
+++ src/LanguageIdentification.FastText.Native/FastTextNativeLibrary.cs
@@ -5,11 +5,6 @@ namespace LanguageIdentification.FastText.Native
     public class FastTextNativeLibrary
     {
         public const string Name = "libfasttext.so";
-
-        public static void LoadNativeLibrary()
-        {
-            NativeLibrary.Load(Name);
-        }
     }
 }
 
