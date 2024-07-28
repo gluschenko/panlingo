503fe665c27c87c2e4562b47b517abe1b0b4d0ae
Alexander Gluschenko <qugluschenko@gmail.com>
2024-07-28 13:01:00 +0400

    Move ISOExtractor to Core

--- src/LangaugeCode.ConsoleTest/Program.cs
+++ src/LangaugeCode.ConsoleTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Panlingo.LangaugeCode.Generator;
+using Panlingo.LangaugeCode.Core.Models;
 
 namespace LangaugeCode.ConsoleTest
 {
@@ -11,6 +12,17 @@ namespace LangaugeCode.ConsoleTest
             var a = await extractor.ExtractLangaugeCodesSetTwoAsync();
             var b = await extractor.ExtractLangaugeCodesSetThreeAsync();
             var c = await extractor.ExtractLanguageCodeDeprecationsSetTwoAsync();
+
+            var aaa = new ISOGeneratorResources 
+            {
+                A = a,
+                B = b,
+                C = c,
+            };
+
+            var json = aaa.ToJson();
+
+            var bbb = ISOGeneratorResources.FromJson(json);
             
             ;
         }
---
+++ src/LangaugeCode.Core/Models/ISOGeneratorResources.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Panlingo.LangaugeCode.Core.Models;
+using Panlingo.LanguageCode.Core.Models;
+
+namespace Panlingo.LangaugeCode.Core.Models
+{
+    public class ISOGeneratorResources
+    {
+        public IEnumerable<SetTwoLanguageDescriptor> A { get; set; }
+        public IEnumerable<SetThreeLanguageDescriptor> B { get; set; }
+        public IEnumerable<SetTwoLanguageDeprecationDescriptor> C { get; set; }
+
+        public string ToJson()
+        {
+            var json = JsonSerializer.Serialize(
+                value: this, 
+                options: new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                }
+            );
+
+            return json;
+        }
+
+        public static ISOGeneratorResources FromJson(string json)
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<ISOGeneratorResources>(json)
+                ?? throw new Exception($"Failed to decode {nameof(ISOGeneratorResources)}");
+        }
+    }
+}
