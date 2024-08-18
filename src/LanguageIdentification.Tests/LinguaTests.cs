d74099c8738d2c1a6d6c10fbbe8cea2a4f1c1eac
Alexander Gluschenko <qugluschenko@gmail.com>
2024-08-19 00:16:00 +0400

    Create LinguaTests.cs

---
+++ src/LanguageIdentification.Tests/LinguaTests.cs
@@ -0,0 +1,24 @@
+﻿using Panlingo.LanguageIdentification.Lingua;
+
+namespace Panlingo.LanguageIdentification.Tests;
+
+public class LinguaTests
+{
+    [Theory]
+    [InlineData(LinguaLanguage.English, "Hello, how are you?")]
+    [InlineData(LinguaLanguage.Ukrainian, "Привіт, як справи?")]
+    [InlineData(LinguaLanguage.Russian, "Привет, как дела?")]
+    public void LinguaSingleLanguage(LinguaLanguage languageCode, string text)
+    {
+        using var whatlang = new LinguaDetector(Enum.GetValues<LinguaLanguage>());
+
+        var prediction = whatlang.PredictLanguage(text: text);
+
+        if (prediction is null)
+        {
+            throw new NullReferenceException();
+        }
+
+        Assert.Equal(languageCode, prediction.Language);
+    }
+}
