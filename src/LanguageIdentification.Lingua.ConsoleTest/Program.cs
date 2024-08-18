2795e1cfd5f2fe1b9ec4ffb666e93722dd5efb53
Alexander Gluschenko <qugluschenko@gmail.com>
2024-08-19 00:09:13 +0400

    Test project for Lingua

--- src/LanguageIdentification.Lingua.ConsoleTest/Program.cs
+++ src/LanguageIdentification.Lingua.ConsoleTest/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace LanguageIdentification.Lingua.ConsoleTest
+﻿namespace Panlingo.LanguageIdentification.Lingua.ConsoleTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var whatlang = new LinguaDetector(Enum.GetValues<LinguaLanguage>());
+
+            var texts = new[]
+            {
+                "Hello, how are you?",
+                "Привіт, як справи?",
+                "Привет, как дела?",
+            };
+
+            var predictions = texts
+                .Select(x => new
+                {
+                    Text = x,
+                    Prediction = whatlang.PredictLanguage(x),
+                })
+                .ToArray();
+
+            foreach (var x in predictions)
+            {
+                Console.WriteLine(
+                    $"Text: {x.Text}, " +
+                    $"Language: {x.Prediction?.Language.ToString() ?? "NULL"}, " +
+                    $"Probability: {x.Prediction?.Confidence.ToString() ?? "NULL"}"
+                );
+            }
+
+            var code1 = whatlang.GetLanguageCode(LinguaLanguage.Ukrainian);
+
+            ;
         }
     }
 }
