81714b8284e5a7b2398be04142e6e67bca1ee790
Alexander Gluschenko <qugluschenko@gmail.com>
2024-08-31 00:34:24 +0400

    Update Program.cs

--- src/LanguageIdentification.Lingua.ConsoleTest/Program.cs
+++ src/LanguageIdentification.Lingua.ConsoleTest/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            using var linguaBuilder = new LinguaDetectorBuilder(Enum.GetValues<LinguaLanguage>());
-            linguaBuilder.WithPreloadedLanguageModels();
-            linguaBuilder.WithMinimumRelativeDistance(0.5);
-            linguaBuilder.WithLowAccuracyMode();
+            using var linguaBuilder = new LinguaDetectorBuilder(Enum.GetValues<LinguaLanguage>())
+                .WithPreloadedLanguageModels()
+                .WithMinimumRelativeDistance(0.95)
+                .WithLowAccuracyMode();
 
             using var lingua = linguaBuilder.Build();
 
