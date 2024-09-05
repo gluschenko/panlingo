48efdd4f2ba8618312bed0a47cb304f2eb69a679
Alexander Gluschenko <qugluschenko@gmail.com>
2024-09-05 20:54:19 +0400

    Update setup-build.sh

--- src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
+++ src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
@@ -6,5 +6,5 @@ echo "Installing build packages";
 sudo apt -y update | apt -y update
 sudo apt -y install cmake | apt -y install cmake
 sudo apt -y install g++ | apt -y install g++
-sudo apt -y install protobuf-compiler | apt -y install protobuf-compiler
+sudo apt -y install protobuf-compiler libprotobuf-dev | apt -y install protobuf-compiler libprotobuf-dev
 
