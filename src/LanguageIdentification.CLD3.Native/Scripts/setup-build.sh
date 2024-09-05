5aed615b25dd5e3d8ce982d26d8b4b1f76aa7730
Alexander Gluschenko <qugluschenko@gmail.com>
2024-09-06 01:53:23 +0400

    Update setup-build.sh

--- src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
+++ src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
@@ -9,16 +9,6 @@ sudo apt -y install g++ | apt -y install g++
 sudo apt -y install git | apt -y install git
 # sudo apt -y install protobuf-compiler libprotobuf-dev | apt -y install protobuf-compiler libprotobuf-dev
 
-git clone https://github.com/abseil/abseil-cpp.git
-cd abseil-cpp
-mkdir build
-cd build
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
-make -j $(nproc)
-make install
-
-cd ../..
-
 git clone --recurse-submodules --depth 1 --branch v28.0 https://github.com/protocolbuffers/protobuf.git
 cd protobuf
 mkdir build
