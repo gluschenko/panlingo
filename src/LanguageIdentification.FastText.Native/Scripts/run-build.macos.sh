609865f4132eba8712d0489f0beea713e358f023
Alexander Gluschenko <qugluschenko@gmail.com>
2024-10-02 00:52:17 +0400

    FT: fix MacOS build (-static-libgcc remove)

---
+++ src/LanguageIdentification.FastText.Native/Scripts/run-build.macos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+echo "Hello world";
+
+workspace="build_temp"
+
+mkdir "$workspace" -p
+cp -a ../../third_party/fastText/. $workspace/fasttext
+cp -a Native/. $workspace
+
+ls -R .
+
+cd "$workspace"
+
+mkdir build
+cd build
+
+# Build for MacOS
+rm -rf *
+cmake ..
+make -j $(nproc) # make
+
+ls -R
+
+ldd libfasttext.dylib
+cp libfasttext.dylib ../../libfasttext.dylib
+
+# Clean up
+rm -rf "$workspace"
+echo "Goodbye world";
