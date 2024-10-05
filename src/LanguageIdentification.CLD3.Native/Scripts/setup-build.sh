98330a34478385d4125274fe2bae96532728f55a
Alexander Gluschenko <qugluschenko@gmail.com>
2024-10-06 01:54:31 +0400

    Update setup-build.sh

--- src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
+++ src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
@@ -3,15 +3,15 @@ set -e
 
 echo "Installing build packages";
 
-
+apt -y update
+apt -y install curl
+curl -sL https://deb.nodesource.com/setup_20.x | bash -
 
 apt -y update
 apt -y install cmake
 apt -y install g++
 apt -y install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
 
-apt -y install curl
-curl -sL https://deb.nodesource.com/setup_20.x | bash -
 apt -y install nodejs
 apt -y install npm 
 
