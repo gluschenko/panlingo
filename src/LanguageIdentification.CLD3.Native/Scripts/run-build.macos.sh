4e5965a5ce3dc0ddcbd5e52adfc0ee4c11d1af79
Alexander Gluschenko <qugluschenko@gmail.com>
2024-10-05 23:01:49 +0400

    CLD2 + CLD3: try to build for MacOS

---
+++ src/LanguageIdentification.CLD3.Native/Scripts/run-build.macos.sh
@@ -0,0 +1,48 @@
+﻿#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    echo "Error: No architecture specified."
+    echo "Usage: $0 <arch>"
+    exit 1
+fi
+
+ARCH=$1
+
+if [[ "$ARCH" != "x86_64" && "$ARCH" != "arm64" ]]; then
+    echo "Error: Invalid architecture specified. Use 'x86_64' or 'arm64'."
+    exit 1
+fi
+
+echo "Hello world $ARCH";
+
+brew install llvm
+
+workspace="build_temp"
+
+mkdir "$workspace" -p
+cp -a ../../third_party/cld3/. $workspace/.
+cp -a Native/. $workspace
+
+ls -R .
+
+cd "$workspace"
+
+zx ../Native/monkey-patch.mjs
+
+mkdir build
+cd build
+
+echo "Build for MacOS on $ARCH";
+rm -rf *
+cmake -DCMAKE_OSX_ARCHITECTURES=$ARCH ..
+make -j $(sysctl -n hw.logicalcpu) 
+
+ls -R
+
+otool -L libcld3.dylib
+cp libcld3.dylib ../../libcld3.$ARCH.dylib
+
+# Clean up
+rm -rf "$workspace"
+echo "Goodbye world";
