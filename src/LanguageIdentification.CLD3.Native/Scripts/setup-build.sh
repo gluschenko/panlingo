b59fbca3cf7e877e738620c050b5855a7017ea3e
Alexander Gluschenko <qugluschenko@gmail.com>
2024-10-06 01:22:51 +0400

    Update setup-build.sh

--- src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
+++ src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
@@ -3,16 +3,19 @@ set -e
 
 echo "Installing build packages";
 
-apt-get update && apt-get install -y ca-certificates curl gnupg && \
-    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg && \
-    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
 
 apt -y update
 apt -y install cmake
 apt -y install g++
 apt -y install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
+
+apt -y install curl
+curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
 apt -y install nodejs
 apt -y install npm 
+
+node --version
 
 npm install -g zx
 
