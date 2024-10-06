8348b293d19644015c10feafb3cc16c0719d0386
Alexander Gluschenko <qugluschenko@gmail.com>
2024-10-06 23:24:57 +0400

    Update run-build.sh

--- src/LanguageIdentification.CLD3.Native/Scripts/run-build.sh
+++ src/LanguageIdentification.CLD3.Native/Scripts/run-build.sh
@@ -14,6 +14,8 @@ ls -R .
 cd "$workspace"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 22
 nvm use 22
 npm install -g zx
