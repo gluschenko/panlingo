43b78d893f7ef8e266a591686054b92330a4d3ea
Alexander Gluschenko <qugluschenko@gmail.com>
2024-10-06 22:17:43 +0400

    Update setup-build.sh

--- src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
+++ src/LanguageIdentification.CLD3.Native/Scripts/setup-build.sh
@@ -8,14 +8,13 @@ apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.bashrc
 nvm install 22
+npm install -g zx
+source ~/.bashrc
 
 apt -y update
 apt -y install cmake
 apt -y install g++
 apt -y install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64
 
-node --version
-npm --version
 
-npm install -g zx
 
