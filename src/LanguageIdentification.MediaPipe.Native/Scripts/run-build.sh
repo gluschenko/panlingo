8d8a242c45ad22bc389b1a141c4be64707137a95
Alexander Gluschenko <qugluschenko@gmail.com>
2024-11-10 00:38:41 +0400

    Update run-build.sh

--- src/LanguageIdentification.MediaPipe.Native/Scripts/run-build.sh
+++ src/LanguageIdentification.MediaPipe.Native/Scripts/run-build.sh
@@ -18,19 +18,10 @@ bazel-6.1.1 build -c opt \
     --define MEDIAPIPE_DISABLE_GPU=1 \
     --define='absl=0' \
     --sandbox_debug --verbose_failures \
-    --config darwin_arm64 \
-    //mediapipe/tasks/c/text/language_detector:liblanguage_detector.dylib
-
-cp ./bazel-bin/mediapipe/tasks/c/text/language_detector/liblanguage_detector.dylib ../liblanguage_detector.dylib
-
-bazel-6.1.1 build -c opt \
-    --linkopt -s --strip always \
-    --define MEDIAPIPE_DISABLE_GPU=1 \
-    --define='absl=0' \
-    --sandbox_debug --verbose_failures \
     //mediapipe/tasks/c/text/language_detector:liblanguage_detector.so
 
 cp ./bazel-bin/mediapipe/tasks/c/text/language_detector/liblanguage_detector.so ../liblanguage_detector.so
+cd ..
 rm -rf "$workspace"
 ldd liblanguage_detector.so
 
