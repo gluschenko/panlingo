97a63e7aae2fa2f27aa6e3d6524368cb10e20a98
Alexander Gluschenko <qugluschenko@gmail.com>
2025-05-14 00:18:04 +0400

    Update monkey-patch.mjs

--- src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
+++ src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
@@ -30,7 +30,7 @@ function a()
 function b()
 {
     const oldText = 'build:linux --define=xnn_enable_avx512amx=false\n\n';
-    const newText = 'build:linux --define=xnn_enable_avx512amx=false\nbuild:linux --define=xnn_enable_avx512fp16=false\n\n';
+    const newText = 'build:linux --define=xnn_enable_avx512amx=false\nbuild:linux --define=xnn_enable_avx512fp16=false\nbuild:linux --define xnn_enable_avxvnniint8=false\n\n';
 
     findAndPatch(".bazelrc", oldText, newText);
 }
