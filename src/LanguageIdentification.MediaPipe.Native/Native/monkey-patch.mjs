66db2da1c3cbb1748315ee1a3c21393096517654
Alexander Gluschenko <qugluschenko@gmail.com>
2025-05-24 21:30:45 +0400

    Update monkey-patch.mjs

--- src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
+++ src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
@@ -1,13 +1,17 @@
 ﻿#!/usr/bin/env zx
 
-async function findAndPatch(endsWith, oldText, newText) {
+async function findAndPatch(endsWith, changes) {
     const files = fs.readdirSync(".", { recursive: true });
 
     for (const file of files.filter(x => x.endsWith(endsWith))) {
         console.log("Found: " + file);
 
         const content = await fs.readFile(file, 'utf-8');
-        const newContent = content.replace(oldText, newText);
+        let newContent = content;
+
+        for (const { a, b } of changes) {
+            newContent = newContent.replace(a, b);
+        }
 
         if (content !== newContent) {
             await fs.writeFile(file, newContent);
@@ -24,41 +28,35 @@ function a()
     const oldText = '\{LATEST_METADATA_PARSER_VERSION\}';
     const newText = '1.5.0';
 
-    findAndPatch(".h.template", oldText, newText);
+    findAndPatch(".h.template", [
+        { a: oldText, b: newText },
+    ]);
 }
 
 function b()
 {
     const lineEnding = "\\r?\\n";
 
-    const oldText = new RegExp(`build:linux --define=xnn_enable_avx512amx=false${lineEnding}`, 'g');
-    const newText = `build:linux --define=xnn_enable_avx512amx=false\nbuild:linux --define=xnn_enable_avx512fp16=false\nbuild:linux --define=xnn_enable_avxvnniint8=false\n\n`;
+    const oldTextA = new RegExp(`build:linux --define=xnn_enable_avx512amx=false${lineEnding}`, 'g');
+    const newTextA = `build:linux --define=xnn_enable_avx512amx=false\nbuild:linux --define=xnn_enable_avx512fp16=false\nbuild:linux --define=xnn_enable_avxvnniint8=false\n\n`;
 
-    findAndPatch(".bazelrc", oldText, newText);
-}
+    const oldTextB = new RegExp(`build:windows --host_copt=/D_USE_MATH_DEFINES${lineEnding}`, 'g');
+    const newTextB = `build:windows --host_copt=/D_USE_MATH_DEFINES\nbuild:windows --define=xnn_enable_avx512amx=false\nbuild:windows --define=xnn_enable_avx512fp16=false\nbuild:windows --define=xnn_enable_avxvnni=falsee\nbuild:windows --define=xnn_enable_avxvnniint8=false\n\n`;
 
-function c() {
-    const lineEnding = "\\r?\\n";
+    const oldTextC = new RegExp(`-std=c\\+\\+17`, 'g');
+    const newTextC = `-std=c++20`;
 
-    const oldText = new RegExp(`build:windows --host_copt=/D_USE_MATH_DEFINES${lineEnding}`, 'g');
-    const newText = `build:windows --host_copt=/D_USE_MATH_DEFINES\nbuild:windows --define=xnn_enable_avx512amx=false\nbuild:windows --define=xnn_enable_avx512fp16=false\nbuild:windows --define=xnn_enable_avxvnni=falsee\nbuild:windows --define=xnn_enable_avxvnniint8=false\n\n`;
-
-    findAndPatch(".bazelrc", oldText, newText);
-}
-
-function d() {
-    const oldText = new RegExp(`-std=c\\+\\+17`, 'g');
-    const newText = `-std=c++20`;
-
-    findAndPatch(".bazelrc", oldText, newText);
+    findAndPatch(".bazelrc", [
+        { a: oldTextA, b: newTextA },
+        { a: oldTextB, b: newTextB },
+        { a: oldTextC, b: newTextC },
+    ]);
 }
 
 console.log('[Monkey patching is started]');
 
 await a();
 await b();
-await c();
-await d();
 
 console.log('[Monkey patching is done]');
 
