835e51ca5679a691efa0d54d9f20377b045c27d8
Alexander Gluschenko <qugluschenko@gmail.com>
2025-06-22 19:15:09 +0400

    Update run-tests.ps1

--- src/run-tests.ps1
+++ src/run-tests.ps1
@@ -11,6 +11,4 @@ docker exec ${container_name} sh -c "cd /src && dotnet test -c ReleaseLinuxOnly 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
-docker container stop ${container_name}
-docker container rm ${container_name}
-docker rm ${image_name} --force
+docker rm ${container_name} --force
