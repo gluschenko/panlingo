c1b4f1216d83896c754e619c975210dba30e92c8
Alexander Gluschenko <qugluschenko@gmail.com>
2025-09-04 01:13:31 +0400

    MediaPipe: --linkopt=libvcruntime.lib  build:windows  --linkopt=/NODEFAULTLIB:ucrtbase.lib

--- src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
+++ src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
@@ -55,7 +55,10 @@ build:windows --define=xnn_enable_avxvnniint8=false
 build:windows --cxxopt=/MT
 build:windows --host_cxxopt=/MT
 build:windows --linkopt=libucrt.lib
+build:windows --linkopt=libvcruntime.lib
+build:windows --linkopt=libcmt.lib
 build:windows --linkopt=/NODEFAULTLIB:MSVCRT
+build:windows --linkopt=/NODEFAULTLIB:ucrtbase.lib
 
     `;
 
