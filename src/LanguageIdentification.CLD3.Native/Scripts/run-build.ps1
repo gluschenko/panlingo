18799429c47168ecc73c90902dfa72d2766a70d2
Alexander Gluschenko <qugluschenko@gmail.com>
2025-09-03 04:44:13 +0400

    CLD2 + CLD3: fixes

--- src/LanguageIdentification.CLD3.Native/Scripts/run-build.ps1
+++ src/LanguageIdentification.CLD3.Native/Scripts/run-build.ps1
@@ -27,7 +27,7 @@ if (-Not (Test-Path "build")) {
 Set-Location "build"
 
 choco install nvm
-$env:Path += ";C:\ProgramData\chocolatey\lib"
+$env:Path += ";C:\ProgramData\chocolatey\bin"
 nvm install 22
 nvm use 22
 
