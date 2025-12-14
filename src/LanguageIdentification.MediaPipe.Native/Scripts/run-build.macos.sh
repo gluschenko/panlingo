e406ea46d45b5a1de57e185c23b7c3c034f8d150
Alexander Gluschenko <qugluschenko@gmail.com>
2025-12-15 03:47:19 +0400

    Remove Python setup steps from macOS build script
    Eliminated Python 3.11 installation, virtual environment setup, and related pip package installations from run-build.macos.sh. The script now focuses on Homebrew updates and Node.js tooling, streamlining the build process.

--- src/LanguageIdentification.MediaPipe.Native/Scripts/run-build.macos.sh
+++ src/LanguageIdentification.MediaPipe.Native/Scripts/run-build.macos.sh
@@ -19,41 +19,6 @@ echo "Hello world $ARCH";
 brew update
 HOMEBREW_NO_AUTO_UPDATE=1
 
-# Python
-PYTHON311_BIN="/opt/homebrew/opt/python@3.11/bin/python3.11"
-
-if [ ! -x "$PYTHON311_BIN" ]; then
-    echo "Installing Python 3.11..."
-    brew install python@3.11
-fi
-
-echo -n "Using Python binary: " && echo "$PYTHON311_BIN"
-echo -n "Python version: " && "$PYTHON311_BIN" --version
-
-# Virtual environment
-"$PYTHON311_BIN" -m venv venv
-source venv/bin/activate
-
-python --version
-python -m pip install --upgrade pip
-
-pip install --break-system-packages --upgrade setuptools wheel future absl-py
-pip install --break-system-packages --upgrade tensorflow-macos==2.16.2 tf_slim
-pip install --break-system-packages --upgrade "numpy<2" jax['cpu'] opencv-contrib-python protobuf==3.20.1 six==1.14.0
-pip install --break-system-packages --upgrade opencv-contrib-python protobuf==3.20.1 six==1.14.0
-
-# Other bullshit
-brew uninstall cmake
-brew install cmake
-brew install protobuf@29
-brew install git 
-brew install wget 
-brew install pkg-config 
-brew install curl
-brew install ffmpeg
-brew install bazelisk
-brew install llvm@16
-
 npm install -g zx
 
 echo -n "Bazel: " && bazel version
