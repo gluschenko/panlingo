17ee3b0553c0ea851497772cef9b808ee218c328
Alexander Gluschenko <qugluschenko@gmail.com>
2026-06-13 02:13:19 +0400

    Update monkey-patch.mjs

--- src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
+++ src/LanguageIdentification.MediaPipe.Native/Native/monkey-patch.mjs
@@ -123,6 +123,10 @@ function e() {
 }
 
 function f() {
+    if (process.platform !== "linux") {
+        return;
+    }
+
     const oldText = "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30";
     const newText = "ff4ab312d6cc18542ea01941f1fc450ee1c0150240d6cab183ca9bb36e5eaf1a";
 
